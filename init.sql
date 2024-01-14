9ad05286d9734bc3da72a57fd6a0bb05667689a7
KJ-TAM-QR4M3\deepadmin <kaijun.tam@ncs.com.sg>
2024-01-14 11:32:45 +0800

    EDIT|NO JIRA|update db constraint

--- init.sql
+++ init.sql
@@ -1,14 +1,12 @@
--- Create the database
 CREATE DATABASE IF NOT EXISTS inventory_management;
 
--- Switch to the database
 USE inventory_management;
 
--- Create the table
 CREATE TABLE IF NOT EXISTS t_product_item (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
+    name VARCHAR(255) UNIQUE,
     category VARCHAR(50),
     price VARCHAR(20),
-    last_updated_dt DATETIME
+    last_updated_dt DATETIME,
+    INDEX idx_last_updated_dt (last_updated_dt)
 );

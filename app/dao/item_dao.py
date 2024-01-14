e38a0745ce7e080aac8d9f60d9e941dc6b3f9aa6
KJ-TAM-QR4M3\deepadmin <kaijun.tam@ncs.com.sg>
2024-01-14 10:06:01 +0800

    EDIT|NO JIRA|refactor item_dao.py

--- app/dao/item_dao.py
+++ app/dao/item_dao.py
@@ -1,23 +1,26 @@
 from app.models.item import Item
 from app.utils.db_utils import get_database_connection
 
+SELECT_ITEM_BY_NAME = "SELECT * FROM t_product_item WHERE name = %s"
+UPDATE_ITEM_QUERY = "UPDATE t_product_item SET price=%s, last_updated_dt=NOW() WHERE name=%s;"
+INSERT_ITEM_QUERY = "INSERT INTO t_product_item (name, category, price, last_updated_dt) VALUES (%s, %s, %s, NOW());"
+SELECT_ALL_ITEMS_QUERY = "SELECT id, name, category, price, last_updated_dt FROM t_product_item"
+SELECT_ITEMS_BY_DT_WHERE_CLAUSE_QUERY = " WHERE last_updated_dt BETWEEN %s AND %s"
+SELECT_ITEMS_BY_CATEGORY_WHERE_CLAUSE_QUERY = " WHERE category = %s"
+
 
 def item_exists(name):
     with get_database_connection() as (_, cursor):
-        select_query = "SELECT * FROM t_product_item WHERE name = %s"
-        cursor.execute(select_query, (name,))
+        cursor.execute(SELECT_ITEM_BY_NAME, (name,))
         return cursor.fetchone() is not None
 
 
 def update_item(new_item):
     with get_database_connection() as (connection, cursor):
-        update_query = "UPDATE t_product_item SET price=%s, last_updated_dt=NOW() WHERE name=%s;"
         update_values = (new_item.price, new_item.name)
-        cursor.execute(update_query, update_values)
+        cursor.execute(UPDATE_ITEM_QUERY, update_values)
 
-        select_query = "SELECT id FROM t_product_item WHERE name = %s"
-        cursor.execute(select_query, (new_item.name,))
-
+        cursor.execute(SELECT_ITEM_BY_NAME, (new_item.name,))
         result = cursor.fetchone()
 
         connection.commit()
@@ -27,9 +30,8 @@ def update_item(new_item):
 
 def create_item(new_item):
     with get_database_connection() as (connection, cursor):
-        insert_query = "INSERT INTO t_product_item (name, category, price, last_updated_dt) VALUES (%s, %s, %s, NOW());"
         insert_values = (new_item.name, new_item.category, new_item.price)
-        cursor.execute(insert_query, insert_values)
+        cursor.execute(INSERT_ITEM_QUERY, insert_values)
 
         connection.commit()
 
@@ -38,37 +40,20 @@ def create_item(new_item):
 
 def get_items():
     with get_database_connection() as (_, cursor):
-        query = (
-            "SELECT id, name, category, price, last_updated_dt FROM t_product_item "
-        )
-
-        cursor.execute(query)
-
+        cursor.execute(SELECT_ALL_ITEMS_QUERY)
         return [Item(id, name, category, price, last_updated) for id, name, category, price, last_updated in
                 cursor.fetchall()]
 
 
 def get_items_by_dt(dt_from, dt_to):
     with get_database_connection() as (_, cursor):
-        query = (
-            "SELECT id, name, category, price, last_updated_dt FROM t_product_item "
-            "WHERE last_updated_dt BETWEEN %s AND %s"
-        )
-
-        cursor.execute(query, (dt_from, dt_to))
-
+        cursor.execute(f"{SELECT_ALL_ITEMS_QUERY}{SELECT_ITEMS_BY_DT_WHERE_CLAUSE_QUERY}", (dt_from, dt_to))
         return [Item(id, name, category, price, last_updated) for id, name, category, price, last_updated in
                 cursor.fetchall()]
 
 
 def get_items_by_category(category):
     with get_database_connection() as (_, cursor):
-        query = (
-            "SELECT id, name, category, price, last_updated_dt FROM t_product_item "
-            "WHERE category = %s"
-        )
-
-        cursor.execute(query, (category,))
-
+        cursor.execute(f"{SELECT_ALL_ITEMS_QUERY}{SELECT_ITEMS_BY_CATEGORY_WHERE_CLAUSE_QUERY}", (category,))
         return [Item(id, name, category, price, last_updated) for id, name, category, price, last_updated in
                 cursor.fetchall()]
